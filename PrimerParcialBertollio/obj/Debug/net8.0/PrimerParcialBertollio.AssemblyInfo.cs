9fe57fe422913846fafad9721b6376473bc75d07
Roberto Bertollio <roberca1978@gmail.com>
2025-04-29 21:11:25 -0300

    Primer correccion al parcial

--- PrimerParcialBertollio/obj/Debug/net8.0/PrimerParcialBertollio.AssemblyInfo.cs
+++ PrimerParcialBertollio/obj/Debug/net8.0/PrimerParcialBertollio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimerParcialBertollio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+719b3126670b79ae0969ca224d644fc9c8f96d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimerParcialBertollio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimerParcialBertollio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
